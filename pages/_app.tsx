d6e926d6eef6fd5b57faa970b1501f96bca89c43
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2023-10-26 16:15:29 -0400

    PushGPT

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Home from '../src/compositions/home';
 import type { AppProps } from "next/app";
 import { ThirdwebProvider, rainbowWallet, metamaskWallet } from "@thirdweb-dev/react";
 import "../src/styles/globals.css";
+import '../src/index.css';
 import React from 'react';
 // import DesignSystem from './compositions/design-system';
 
